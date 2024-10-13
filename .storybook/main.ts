a6913dba4e27e75f1494a4aa96059f4a3c7a3ee9
yuta <jp.yuta0131@gmail.com>
2024-10-14 07:53:12 +0900

    add: storybook configs

---
+++ .storybook/main.ts
@@ -0,0 +1,40 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		"@storybook/addon-onboarding",
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-interactions",
+		{
+			name: "@storybook/addon-styling-webpack",
+			options: {
+				rules: [
+					{
+						test: /\.css$/,
+						use: [
+							"style-loader",
+							{
+								loader: "css-loader",
+								options: { importLoaders: 1 },
+							},
+							{
+								// Gets options from `postcss.config.js` in your project root
+								loader: "postcss-loader",
+								options: { implementation: require.resolve("postcss") },
+							},
+						],
+					},
+				],
+			},
+		},
+		"@storybook/addon-themes",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,29 @@
+import "../src/index.css";
+import { withThemeByClassName } from "@storybook/addon-themes";
+
+export const decorators = [
+	withThemeByClassName({
+		themes: {
+			light: "",
+			dark: "dark",
+		},
+		defaultTheme: "light",
+	}),
+];
+
+export const parameters = {
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+	backgrounds: {
+		default: "light",
+		values: [
+			{ name: "light", value: "#ffffff" },
+			{ name: "dark", value: "#1a1a1a" },
+		],
+	},
+};
