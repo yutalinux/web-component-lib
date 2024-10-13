9120b3f7fd4f0340731ba50425c192d35ab345dc
yuta <jp.yuta0131@gmail.com>
2024-10-14 07:43:01 +0900

    add: template files

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+	css: {
+		postcss: {
+			plugins: [tailwindcss, autoprefixer],
+		},
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			name: "index",
+			fileName: "index",
+		},
+		rollupOptions: {
+			external: ["react", "tailwindcss"],
+			output: {
+				globals: {
+					react: "React",
+					tailwindcss: "tailwindcss",
+				},
+			},
+		},
+	},
+});
