1d93673dc46a164e32b3808a342ef62dbaa4c4a8
Yuta <yutalinux@users.noreply.github.com>
2024-10-14 10:24:52 +0900

    add: test

---
+++ src/components/button/index.test.tsx
@@ -0,0 +1,10 @@
+import { expect, test } from "bun:test";
+import { createElement } from "react";
+import { renderToString } from "react-dom/server";
+import Button from ".";
+
+test("Button test", () => {
+  const button = createElement(Button, { onClick: () => {}, label: 'Click me' })
+  const renderedString = renderToString(button)
+  expect(renderedString).toMatchSnapshot()
+})
