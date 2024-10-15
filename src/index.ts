1ec3cba9d3d53a6f9822601841f3bda3684d29f0
Yuta <yutalinux@users.noreply.github.com>
2024-10-15 20:55:06 +0900

    fix: "index.ts"の不具合を修正

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
 import "@/styles";
 
-// export { default as Box } from "./components/box";
-// export { default as Button } from "./components/button";
-// export { default as Card } from "./components/card";
-// export { default as Form } from "./components/form";
-// export { default as HeaderMenu } from "./components/header-menu";
-// export { default as Label } from "./components/label";
-// export { default as Link } from "./components/link";
-// export { default as TextBox } from "./components/text-box";
-// export { default as ToggleButton } from "./components/toggle-button";
-
-export * from "./components/box";
-export * from "./components/button";
-export * from "./components/card";
-export * from "./components/form";
-export * from "./components/header-menu";
-export * from "./components/label";
-export * from "./components/link";
-export * from "./components/text-box";
-export * from "./components/toggle-button";
+export { default as Box } from "./components/box";
+export { default as Button } from "./components/button";
+export { default as Card } from "./components/card";
+export { default as Form } from "./components/form";
+export { default as HeaderMenu } from "./components/header-menu";
+export { default as Label } from "./components/label";
+export { default as Link } from "./components/link";
+export { default as TextBox } from "./components/text-box";
+export { default as ToggleButton } from "./components/toggle-button";
