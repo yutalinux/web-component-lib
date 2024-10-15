b883bd8cc345112680affdc0b45e5828637bb07b
Yuta <yutalinux@users.noreply.github.com>
2024-10-15 14:02:20 +0900

    refactor: typography関連をリファクタリング

--- src/components/typography/index.test.tsx
+++ src/components/typography/index.test.tsx
@@ -4,21 +4,24 @@ import Typography from ".";
 
 test("ToggleButton Snapshot", () => {
   expect(
-    snapshotElement(Typography, { type: 1, label: "Heading1" }),
+    snapshotElement(Typography, { type: "h1", label: "Heading1" }),
   ).toMatchSnapshot();
   expect(
-    snapshotElement(Typography, { type: 2, label: "Heading2" }),
+    snapshotElement(Typography, { type: "h2", label: "Heading2" }),
   ).toMatchSnapshot();
   expect(
-    snapshotElement(Typography, { type: 3, label: "Heading3" }),
+    snapshotElement(Typography, { type: "h3", label: "Heading3" }),
   ).toMatchSnapshot();
   expect(
-    snapshotElement(Typography, { type: 4, label: "Heading4" }),
+    snapshotElement(Typography, { type: "h4", label: "Heading4" }),
   ).toMatchSnapshot();
   expect(
-    snapshotElement(Typography, { type: 5, label: "Heading5" }),
+    snapshotElement(Typography, { type: "h5", label: "Heading5" }),
   ).toMatchSnapshot();
   expect(
-    snapshotElement(Typography, { type: 6, label: "Heading6" }),
+    snapshotElement(Typography, { type: "h6", label: "Heading6" }),
+  ).toMatchSnapshot();
+  expect(
+    snapshotElement(Typography, { type: "p", label: "Heading6" }),
   ).toMatchSnapshot();
 });
