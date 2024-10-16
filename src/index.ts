6d35101c611a5c4d471a6d753a73f69938a01f41
Yuta <yutalinux@users.noreply.github.com>
2024-10-16 15:12:44 +0900

    style: フォーマット

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "@/styles";
 export * from "./components/box";
 export * from "./components/button";
 export * from "./components/card";
-export * from "./components/dark-mode-menu"
+export * from "./components/dark-mode-menu";
 export * from "./components/form";
 export * from "./components/header-menu";
 export * from "./components/label";
