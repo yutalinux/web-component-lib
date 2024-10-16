0ed892f096b9d09e3e148b4870085d9176c67899
Yuta <yutalinux@users.noreply.github.com>
2024-10-16 20:27:10 +0900

    refactor: ダークモード関連をリファクタリング

---
+++ src/components/dark-mode/button.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { Menu, MenuItem } from "../menu";
+import { MdDarkMode, MdLightMode, MdMonitor } from "react-icons/md";
+import { useDarkMode, type Theme } from ".";
+import { type ReactNode, useEffect, useState } from "react";
+
+export function DarkModeButton() {
+  const [getDarkMode, setDarkMode] = useDarkMode();
+  const [icon, setIcon] = useState<ReactNode>(null);
+  const theme = getDarkMode();
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    switch (theme) {
+      case "system":
+        setDarkMode("system");
+        setIcon(<MdMonitor />);
+        break;
+      case "dark":
+        setDarkMode("dark");
+        setIcon(<MdDarkMode />);
+        break;
+      case "light":
+        setDarkMode("light");
+        setIcon(<MdLightMode />);
+        break;
+    }
+  }, [theme]);
+
+  return (
+    <Menu buttonChildren={icon} icon={true} color={"secondary"}>
+      <MenuItem
+        color="secondary"
+        onAction={() => setDarkMode("system")}
+        label="System"
+      />
+      <MenuItem
+        color="secondary"
+        onAction={() => setDarkMode("dark")}
+        label="Dark"
+      />
+      <MenuItem
+        color="secondary"
+        onAction={() => setDarkMode("light")}
+        label="Light"
+      />
+    </Menu>
+  );
+}
---
+++ src/components/dark-mode/index.test.tsx
@@ -0,0 +1,9 @@
+import { snapshotElement } from "@/utils/snapshot";
+import { expect, test } from "vitest";
+import { DarkModeButton } from "./button";
+
+test("Button Snapshot", () => {
+  // expect(snapshotElement(DarkModeButton, {})).toMatchSnapshot();
+  // expect(snapshotElement(DarkModeButton, { size: "small" })).toMatchSnapshot();
+  // expect(snapshotElement(DarkModeButton, { size: "large" })).toMatchSnapshot();
+});
